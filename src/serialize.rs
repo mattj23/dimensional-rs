91ce9b9ca17b8797245e362b10a3695443c9f7d8
Matt Jarvis <mattj23@gmail.com>
2023-03-04 00:09:04 -0500

    serialization of vector list

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,7 +1,8 @@
 use ncollide2d::na::{Point2, Vector2};
 use ncollide2d::query::Ray;
 use ncollide2d::shape::Ball;
-use serde::Serialize;
+use serde::{Serialize, Serializer};
+use itertools::Itertools;
 
 #[derive(Serialize)]
 #[serde(remote = "Point2<f64>")]
@@ -31,4 +32,48 @@ pub struct Ray2f64 {
 
     #[serde(with = "Vector2f64")]
     dir: Vector2<f64>,
+}
+
+pub struct VectorList2f64 {
+    vectors: Vec<Vector2<f64>>
+}
+
+impl VectorList2f64 {
+    pub fn from_vectors(vectors: &[Vector2<f64>]) -> Self {
+        VectorList2f64 { vectors: vectors.to_vec() }
+    }
+
+    pub fn from_points(points: &[Point2<f64>]) -> Self {
+        VectorList2f64 { vectors: points.iter().map(|p| p.coords).collect() }
+    }
+}
+
+fn v2_to_str(v: &Vector2<f64>) -> String {
+    format!("{},{}", v.x, v.y)
+}
+
+impl Serialize for VectorList2f64 {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer,
+    {
+        let s = format!("{}", self.vectors.iter().map(|v| v2_to_str(v)).format(";"));
+        serializer.serialize_str(&s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ncollide2d::na::Vector2;
+    use crate::serialize::VectorList2f64;
+    use serde_json;
+
+    #[test]
+    fn test_serialize_veclist() {
+        let vecs = vec![Vector2::new(0.0, 1.0), Vector2::new(2.0, 3.0), Vector2::new(4.0, 5.0)];
+        let list = VectorList2f64::from_vectors(&vecs);
+
+        let str = serde_json::to_string(&list).unwrap();
+        assert_eq!("\"0,1;2,3;4,5\"", str);
+    }
+
 }
